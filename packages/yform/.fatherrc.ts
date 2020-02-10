d4d8042c0764f279753f330be97f1a1a8afaff7a
叶枫 <645381995@qq.com>
2020-02-10 17:49:38 +0800

    修复样式

--- packages/yform/.fatherrc.ts
+++ packages/yform/.fatherrc.ts
@@ -1,87 +1,14 @@
-// export default {
-//   target: 'node',
-//   disableTypeCheck: true,
-//   cjs: { type: 'babel', lazy: true },
-//   extraBabelPlugins: ['@babel/plugin-proposal-optional-chaining'],
-// };
-
-// import { IBundleOptions } from 'father';
-
-// const options: IBundleOptions = {
-//     entry: 'src/index.tsx',
-//     cjs: 'rollup',
-//     esm: 'rollup',
-//     // cjs: 'babel',
-//     // esm: 'babel',
-//     // runtimeHelpers: true,
-//     preCommit: { eslint: true, prettier: true },
-//     // extraBabelPlugins: [['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
-// };
-
-// export default options;
-
-// export default {
-//     target: 'node',
-//     disableTypeCheck: true,
-//     esm: 'babel',
-//     // cjs: { type: 'babel', lazy: true },
-//     extraBabelPlugins: ['@babel/plugin-proposal-optional-chaining'],
-// };
-
-// export default {
-//     // esm: { type: 'rollup' },
-//     // cjs: { type: 'rollup' },
-//     // umd: {
-//     //     name: 'DynamicForm',
-//     //     globals: { react: 'React' },
-//     // },
-//     // disableTypeCheck: true,
-//     // entry: 'packages/father-doc/src/index.tsx',
-//     cjs: 'babel',
-//     esm: 'babel',
-//     // namedExports: {
-//     //   'react-is': [
-//     //     'isFragment',
-//     //   ]
-//     // },
-//     // extraBabelPlugins: [
-//     //     [
-//     //         'babel-plugin-import',
-//     //         { libraryName: 'antd-mobile', libraryDirectory: 'es', style: true },
-//     //     ],
-//     // ],
-// };
-
-// export default {
-//     // entry: 'packages/father-doc/src/index.tsx',
-//     // target: 'node',
-//     // disableTypeCheck: true,
-//     // cjs: { type: 'babel', lazy: true },
-//     cjs: 'babel',
-//     // esm: 'babel',
-//     // entry: 'src/index.tsx',
-//     // doc: { typescript: true },
-//     // esm: 'rollup',
-//     // extraBabelPlugins: ['@babel/plugin-proposal-optional-chaining'],
-
-//     // entry: 'src/index.tsx',
-//     // cjs: 'rollup',
-//     // esm: 'rollup',
-//     // cjs: 'babel',
-//     // esm: 'babel',
-//     // runtimeHelpers: true,
-//     // preCommit: { eslint: true, prettier: true },
-// };
-
-export default {
-  target: 'node',
-  extraBabelPlugins: [
-    ['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
-  ],
+const options = {
+  // entry: 'src/index.tsx',
+  // cjs: 'rollup',
+  // esm: 'rollup',
+  // target: 'node',
   runtimeHelpers: true,
+  cjs: 'babel',
   cssModules: false, // https://github.com/umijs/father/issues/131
-  extractCSS: true,
+  // extractCSS: true,
   lessInBabelMode: true,
-  esm: 'babel',
-  cjs: { type: 'babel', lazy: true },
+  // extraBabelPlugins: [['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
 };
+
+export default options;
