662da73046a8f0a523f200abbdd85a456f3d92c2
叶枫 <645381995@qq.com>
2020-02-11 10:00:18 +0800

    解决 test

--- packages/yform/.fatherrc.ts
+++ packages/yform/.fatherrc.ts
@@ -4,7 +4,7 @@ const options = {
   // esm: 'rollup',
   // target: 'node',
   runtimeHelpers: true,
-  cjs: 'babel',
+  cjs: { type: 'babel', lazy: true },
   cssModules: false, // https://github.com/umijs/father/issues/131
   // extractCSS: true,
   lessInBabelMode: true,
--- setupTests.js
+++ setupTests.js
@@ -1,4 +1,4 @@
-import '@babel/polyfill';
+// import '@babel/polyfill';
 
 const React = require('react');
 
