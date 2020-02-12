e55676f1ecc1ef03afd810042dd1db946a9a10cc
叶枫 <645381995@qq.com>
2020-02-12 16:07:00 +0800

    demo2

--- docs/demo.md
+++ docs/demo.md
@@ -3,6 +3,10 @@ title: Demo
 order: -2
 ---
 
+## 列表页面交互
+
+<code src="./demo/demo2.tsx" />
+
 ## 代码演示
 
 <code src="./demo/demo1.tsx" />
---
+++ docs/demo/demo2.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable no-console */
+import React, { useState, useEffect } from 'react';
+import { YForm } from 'father-doc-yform';
+
+const layout = { labelCol: { span: 4 }, wrapperCol: { span: 20 } };
+
+const Demo = () => {
+  const [data, setData] = useState({});
+  const [disabled, setDisabled] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { formatFieldsValue, onFormatFieldsValue } = YForm.useFormatFieldsValue();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({ name: '张三', age: '10' });
+      setLoading(false);
+    }, 10);
+  }, []);
+
+  onFormatFieldsValue([
+    { name: 'append_field', format: () => '提交前追加字段' },
+    { name: 'name', format: ({ name }) => `${name}_改变了` },
+  ]);
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+  const onSave = (values: any) => {
+    console.log('values:', values);
+  };
+
+  return (
+    <YForm
+      {...layout}
+      loading={loading}
+      initialValues={data}
+      name="basic"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      onSave={onSave}
+      formatFieldsValue={formatFieldsValue}
+      required
+      disabled={disabled}
+    >
+      {[
+        { type: 'input', label: 'name', name: 'name' },
+        { type: 'input', label: 'age', name: 'age', componentProps: { suffix: '岁' } },
+        { type: 'money', label: 'money', name: 'money' },
+        {
+          type: 'submit',
+          componentProps: {
+            showBtns: {
+              showEdit: {
+                onClick: e => {
+                  e.preventDefault();
+                  setDisabled(c => !c);
+                },
+              },
+            },
+          },
+        },
+        {
+          className: 'button-more-left',
+          dataSource: [
+            {
+              type: 'button',
+              noStyle: true,
+              plugins: { disabled: false },
+              componentProps: {
+                type: 'primary',
+                onClick: () => setDisabled(c => !c),
+                children: '状态',
+              },
+            },
+          ],
+        },
+      ]}
+    </YForm>
+  );
+};
+export default Demo;
