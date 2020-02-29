e1a3a70dc391eab51b7dc5a75a0f33ba3ce1a27c
叶枫 <7971419+crazyair@users.noreply.github.com>
2020-02-29 11:34:09 +0800

    feat: add codecov (#9)
    * chore: test
    
    * test
    
    * test
    
    * md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![codecov](https://codecov.io/gh/crazyair/father-doc-yform/branch/master/graph/badge.svg)](https://codecov.io/gh/crazyair/father-doc-yform)
+
 ## Usage & Guide
 
 To check out live examples and docs, visit [yform now](https://father-doc-yform.now.sh/).
--- scripts/getChangeLog.js
+++ scripts/getChangeLog.js
@@ -7,14 +7,14 @@ const stringFromFile = path => {
 };
 
 const text = stringFromFile('./docs/changelog.md');
-const versionLogs = text.split('---\n\n')[1];
+const versionLogs = text;
 
-const allMatchingWords = versionLogs.split('##');
+const allMatchingWords = versionLogs.split('## ');
 let changelog = allMatchingWords[1];
 changelog = changelog.replace(/%/g, '%25');
 changelog = changelog.replace(/\n/g, '%0A');
 changelog = changelog.replace(/\r/g, '%0D');
 
-const demo = `::set-output name=changelog::##${changelog}`;
+const demo = `::set-output name=changelog::## ${changelog}`;
 // eslint-disable-next-line no-console
 console.log(demo);
