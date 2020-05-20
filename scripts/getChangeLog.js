636a791ec0380882cd8d27e89a2a194d82b296b0
黄建峰 <jianfeng.huang@yunzhanghu.com>
2020-05-20 10:31:47 +0800

    docs: doc

--- scripts/getChangeLog.js
+++ scripts/getChangeLog.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 
 const { readFileSync } = fs;
 
-const stringFromFile = path => {
+const stringFromFile = (path) => {
   return readFileSync(path, 'utf-8');
 };
 
 const text = stringFromFile('./docs/changelog.md');
 const versionLogs = text;
 
-const allMatchingWords = versionLogs.split('## ');
+const allMatchingWords = versionLogs.split('## [');
 let changelog = allMatchingWords[1];
 changelog = changelog.replace(/%/g, '%25');
 changelog = changelog.replace(/\n/g, '%0A');
 changelog = changelog.replace(/\r/g, '%0D');
 
-const demo = `::set-output name=changelog::## ${changelog}`;
+const demo = `::set-output name=changelog::## [${changelog}`;
 // eslint-disable-next-line no-console
 console.log(demo);
