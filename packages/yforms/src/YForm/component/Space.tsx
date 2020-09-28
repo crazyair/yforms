446b3765a62731b2f952fd85f84a0d13c8532403
crazyair <645381995@qq.com>
2020-09-28 11:27:44 +0800

    1.3.5

--- packages/yforms/src/YForm/component/Space.tsx
+++ packages/yforms/src/YForm/component/Space.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { map, forEach, isArray, isObject } from 'lodash';
+import { map, isArray } from 'lodash';
 import { Space } from 'antd';
 import { SpaceProps } from 'antd/lib/space';
 
 import YForm from '..';
-import { YFormItemProps, YFormDataSource } from '../Items';
-import { getParentNameData } from '../utils';
+import { YFormItemProps } from '../Items';
 
 export interface YFormSpaceProps extends YFormItemProps {
   componentProps?: SpaceProps;
@@ -14,56 +13,19 @@ export interface YFormSpaceProps extends YFormItemProps {
 
 export default (props: YFormSpaceProps['componentProps']) => {
   const itemProps = React.useContext(YForm.YFormItemContext) as YFormSpaceProps;
-  const { items, scenes, name } = itemProps;
-  // 获取子集所有 shouldUpdate
-  const shouldUpdates = [];
-  forEach(isArray(items) ? items : [items], (item) => {
-    if (isObject(item)) {
-      const { shouldUpdate } = item as YFormDataSource;
-      shouldUpdates.push(shouldUpdate);
-    }
-  });
-  const shouldUpdate = (prevValue, nextValue) => {
-    let isRender = false;
-    // 判断只要子集有个为 true， 则 render
-    forEach(shouldUpdates, (item) => {
-      if (typeof item === 'boolean' && item) {
-        isRender = true;
-      }
-      if (typeof item === 'function' && item(prevValue, nextValue)) {
-        isRender = true;
-      }
-    });
-    return isRender;
-  };
+  const { items, scenes } = itemProps;
+
   return (
-    <YForm.Item noStyle shouldUpdate={shouldUpdate}>
-      {(form) => {
-        return (
-          <Space {...props}>
-            {isArray(items)
-              ? map(items, (item, index) => {
-                  if (isObject(item)) {
-                    const { isShow } = item as YFormDataSource;
-                    const parentValue = getParentNameData(form.getFieldsValue(true), name);
-                    if (
-                      typeof isShow === 'function' &&
-                      !isShow(parentValue, form.getFieldsValue(true))
-                    ) {
-                      return null;
-                    }
-                    if (typeof isShow === 'boolean' && !isShow) return null;
-                  }
-                  return (
-                    <YForm.Items scenes={{ noCol: true, ...scenes }} noStyle key={index}>
-                      {[item]}
-                    </YForm.Items>
-                  );
-                })
-              : items}
-          </Space>
-        );
-      }}
-    </YForm.Item>
+    <Space {...props}>
+      {isArray(items)
+        ? map(items, (item, index) => {
+            return (
+              <YForm.Items scenes={{ noCol: true, ...scenes }} noStyle key={index}>
+                {[item]}
+              </YForm.Items>
+            );
+          })
+        : items}
+    </Space>
   );
 };
